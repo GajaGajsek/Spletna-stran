0ff2d839e82dc1476545979001c2c0d7ade1f9a9
GajaGajsek <162341438+GajaGajsek@users.noreply.github.com>
2025-09-30 11:05:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="sl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/slog.css" rel="stylesheet">
+        <script src="js/izjave.js"></script>
+        <title> Potovalna agencija: Morska dogodivščina  </title>
+
+    </head>
+    <body class="omejeno">
+        <div class="zozan-ovijalni">  
+            <header>   <!-- glava spletne strani-->
+                <div id="logo">
+                    <img src="images/Logo.png" alt="Logotip podjetja">
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#glavna"> Domov </a> </li>
+                        <li> <a href="#predstavitev">Dejavnosti </a> </li>
+                        <li> <a href="#pricevanja"> Pričevanja </a></li>
+                        <li> <a href="#obvestila"> Obvestila  </a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main> <!-- glavna vsebina spletne strani -->
+                <section id="glavna"> <!-- prvi razdelek -->
+                    <div class="ovijalni">
+                        <article class="opis-prvi">
+                            <h1 class="naslov-prvi"> Morska dogodivščina </h1>
+                            <p class="besedilo-prvi">
+                                Potovalna agencija "Morska dogodivščina" vam ponuja nepozabna potovanja in počitnice na Jadranu 
+                                in Mediteranu ter razburljiva križarjenja po čudovitih vodah Sredozemlja. S specializirano ponudbo 
+                                destinacij ob obalah Jadrana in Mediterana, skupaj s kakovostnimi križarjenji, vam agencija omogoča, 
+                                da doživite raznolikost teh območij na najbolj edinstven način. Z izkušenimi svetovalci in strokovnim 
+                                osebjem vam "Morska dogodivščina" zagotavlja popolnoma prilagojene počitniške pakete, ki bodo ustrezali 
+                                vašim željam in pričakovanjem, ter vam omogočajo, da ustvarite nepozabne spomine na svoje potovanje.
+                            </p>
+                        </article>
+                        <div class="grafika-prvi">
+                        </div>
+                    </div>
+                </section>
+
+                <section id="predstavitev">
+                    <div class="ovijalni">
+                        <div class="predstavitev-glava">
+                            <article class="ovijalni-mali">
+                                <h2 class="naslov-razdelka"> Dejavnosti podjetja </h2>
+                                <p class="odstavek-razdelka">
+                                    Podjetje "Morska dogodivščina" je specializirano za organizacijo počitnic na Jadranu in v 
+                                    Mediteranu ter križarjenj po Sredozemlju. Nudijo raznolike morske izkušnje in avanture, ki vključujejo raziskovanje slikovitih 
+                                    obal, obisk skritih zalivov in odkrivanje čarobnosti morskega življenja. S svojim strokovnim znanjem in izkušnjami vam pomagajo 
+                                    ustvariti nepozabne trenutke na morju, bodisi med uživanjem na plaži, raziskovanjem podvodnega sveta ali raziskovanjem kultur
+                                    ob obali. Morska dogodivščina vas vabi, da doživite edinstvene in nepozabne počitnice, obarvane z morjem, soncem in raznolikimi 
+                                    doživetji.
+                                </p>
+                            </article>
+                        </div>
+                        <div class="predstavitev-vsebina">
+                            <article class="predstavitev">
+                                <div class="ikona">
+                                    <img src="images/Jadran_ikona.png" alt="Počitnice na jadranu">
+                                </div>
+                                <h3 class="naslov-predstavitve"> Počitnice na Jadranu </h3>
+                                <p class="odstavek-mali">
+                                    "Počitnice na Jadranu" so celovita storitev, ki vključuje organizacijo dopusta 
+                                    ob obalah Jadranskega morja. To vključuje rezervacijo namestitve, prevoza, izletov in aktivnosti 
+                                    ter podporo strankam med njihovim bivanjem. Strankam omogoča sproščeno uživanje v soncu, morju, 
+                                    kulinariki in lokalni kulturi
+                                </p>
+                            </article>
+
+                            <article class="predstavitev">
+                                <div class="ikona">
+                                    <img src="images/Medetiran_ikona.png" alt="Pošitnice na mediteranu">
+                                </div>
+                                <h3 class="naslov-predstavitve"> Počitnice na Mediteranu  </h3>
+                                <p class="odstavek-mali">
+                                    Počitnice na Mediteranu" so vrhunska turistična storitev, ki strankam 
+                                    omogoča uživanje v čarobnih destinacijah ob obalah Sredozemskega morja. Ta storitev vključuje 
+                                    rezervacijo prestižne namestitve v hotelih, vilah ali apartmajih s pogledom na morje, organizacijo 
+                                    udobnega prevoza do destinacije, raznolike izlete in aktivnosti, kot so obisk zgodovinskih mest, 
+                                    sončenje na čudovitih plažah, okušanje lokalne kulinarike ter razvajanje v wellness centrih ob obali. 
+                                    Poleg tega nudimo tudi turistično svetovanje, podporo strankam med potovanjem in zagotavljajo 
+                                    brezskrbno doživetje počitnic na eni izmed najbolj priljubljenih turističnih destinacij 
+                                    na svetu.
+                                </p>
+                            </article>
+
+                            <article class="predstavitev">
+                                <div class="ikona"> 
+                                    <img src="images/Križarjenje_ikona.png" alt="Križarjanje po Jadranu in Mediteranu">
+                                </div>
+                                <h3 class="naslov-predstavitve"> Križarjenje po Jadranu in Mediteranu </h3>
+                                <p class="odstavek-mali">
+                                    Križarjenje po Jadranu in Sredozemlju" je ekskluzivna turistična storitev, 
+                                    ki strankam omogoča raziskovanje osupljivih destinacij ob obalah Jadranskega morja in 
+                                    Sredozemskega morja z udobjem in razkošjem potovanja po morju. Medtem ko plujejo po kristalno 
+                                    čistem morju, stranke doživijo edinstvene kulturne, zgodovinske in naravne znamenitosti, 
+                                    kot so slikovita obmorska mesta, antične ruševine, čudovite plaže ter slikoviti otoki.
+                                </p>
+                            </article>
+
+                            <article class="predstavitev">
+                                <div class="ikona">
+                                    <img src="images/Potovanja_po_meri_ikona.png" alt="Križarjanje po meri">
+                                </div>
+                                <h3 class="naslov-predstavitve"> Potovanja po meri </h3>
+                                <p class="odstavek-mali">
+                                    Potovanje po meri je prilagojena turistična storitev, ki strankam omogoča popolno prilagoditev 
+                                    njihovega potovanja glede na njihove želje, interese in potrebe. Namesto standardnih turističnih 
+                                    paketov stranke s pomočjo specializiranih potovalnih agencij ali turističnih svetovalcev oblikujejo 
+                                    svoje edinstveno potovanje, ki popolnoma ustreza njihovim pričakovanjem. 
+                                </p>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="pricevanja">
+                    <img  src="images/lik1.png" alt="Lik 1">
+                    <img  src="images/lik2.png" alt="Lik 2">
+                    <img  src="images/lik3.png" alt="Lik 3">
+                    <div class="ovijalni">
+                        <h2 class="naslov-razdelka"> Pričevanja strank </h2>
+                        <div class="pricevanja-ovijalni" id="java_izjave">
+                        </div>
+                    </div>
+                </section>
+
+                <section id="obvestila" class="svetla-pisava">
+                    <div class="ovijalni-mali">
+                        <div class="novice-opis">
+                            <h2 class="naslov-razdelka"> Prijava na novice  </h2>
+                            <p class="odstavek-razdelka">
+                                Prijavite se na novice potovalne agencije "Morska dogodivščina" in bodite med prvimi, 
+                                ki boste izvedeli o najnovejših ponudbah, destinacijah in posebnih popustih. Ne zamudite priložnosti za nepozabne
+                                počitnice ob morju in križarjenja po čudovitem Sredozemlju. Bodite korak pred vsemi in se prijavite zdaj, da 
+                                ostanete informirani in pripravljeni na svojo naslednjo morsko dogodivščino!
+                            </p>                        
+                        </div>
+                        <div class="novice-obrazec">
+                            <form action="" method="get">
+                                <input id = "email" class="novice-vnos" type="email" placeholder="Vpiši svoj e mail naslov" required>
+                                <button class="novice-gumb" type="submit" id="novice-gumb"> Prijava </button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <footer> <!-- noga spletne strani -->
+                <div class="ovijalni noga-znotraj svetla-pisava">
+                    <div class="noga-logo">
+                        <a href="#logo"> <img src="images/Logo.png" alt="Logotip podjetja"> </a>
+                    </div>
+                    <ul class="noga-povezave">
+                        <li> <a href="#glavna">Domov </a> </li>
+                        <li> <a href="#predstavitev"> Dejavnosti </a> </li>
+                        <li> <a href="#pricevanja"> Pričevanja </a> </li>
+                        <li>  <a href="#obvestila"> Obvestila </a></li>
+                    </ul>
+                    <ul class="noga-druzabna">
+                        <li> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffe5ec" fill="none">
+                            <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                            <path d="M16.9265 8.02637H13.9816C12.9378 8.02637 12.0894 8.86847 12.0817 9.91229L11.9964 21.4268M10.082 14.0017H14.8847" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg> </li>
+                        <li> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffe5ec" fill="none">
+                            <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                            <path d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z" stroke="currentColor" stroke-width="1.5" />
+                            <path d="M17.5078 6.5L17.4988 6.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg> </li>
+                        <li> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffe5ec" fill="none">
+                            <path d="M7 10V17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M11 13V17M11 13C11 11.3431 12.3431 10 14 10C15.6569 10 17 11.3431 17 13V17M11 13V10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M7.00801 7L6.99902 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
+                        </svg> </li>
+                    </ul>
+                    <div class="noga-copyright">
+                        &copy; 2023 Morska dogodivščina     
+                    </div>
+                </div>
+            </footer>
+
+        </div> 
+
+
+        <script>
+            function sestava_izjave(element){
+                
+                let figure = document.createElement("figure");
+                figure.className = "izjava";
+
+                let blockquote = document.createElement("blockquote");
+                figure.appendChild(blockquote);
+
+                let besedilo = document.createElement("p");
+                besedilo.className = "izjava-opis";
+                besedilo.textContent = element.besedilo;
+                blockquote.appendChild(besedilo);
+
+                              
+                let slika = document.createElement("img");
+                figure.appendChild(slika);
+                slika.src = element.slika;
+                slika.alt = element.ime_stranke;
+
+                let figcaption = document.createElement("figcaption");
+                figure.appendChild(figcaption);
+                
+                let cite = document.createElement("cite");
+                cite.className = "izjava-glava";
+                cite.textContent = element.ime_stranke;
+                figcaption.appendChild(cite);
+
+                let cas = document.createElement("time");
+                cas.className = "izjava-noga";
+                cas.dateTime = element.datum;
+                cas.textContent = element.datum;
+                figcaption.appendChild(cas);
+
+                return figure;
+            }
+
+            console.log(izjave);
+            izjave.forEach(element => {
+                let stars = document.getElementById("java_izjave");
+                let izjava = sestava_izjave(element);
+                stars.appendChild(izjava);
+                
+            });
+        </script>
+        <script>
+            //let el = document.getElementById("novice-gumb");
+            //console.log(el);
+             document.getElementById("novice-gumb").addEventListener("click",function(event){
+                // prešrečimo pošiljanje ob kliku na gumb
+                //pridobim vrednost elektronskega naslova
+                let email = document.getElementById("email");
+
+
+                if(!email.checkValidity()) {
+                    return;
+                }
+
+                event.preventDefault();
+
+               
+                console.log(email);
+
+                let pozdravnoOkno = document.createElement("div");
+                pozdravnoOkno.classList.add("skrito");
+                pozdravnoOkno.id = "pozdravno_okno";
+
+                let zahvala = document.createElement("h2");
+                zahvala.textContent = "Hvala za prijavo, ";
+
+                let naslov_eposta = document.createElement("span");
+                naslov_eposta.id = "naslov_eposta";
+                naslov_eposta.textContent = email.value
+
+                zahvala.appendChild(naslov_eposta)
+
+                let odstavek_izdelava = document.createElement("p");
+                odstavek_izdelava.textContent = "Stran je v izdelavi";
+
+                let zapri_gumb = document.createElement("button");
+                zapri_gumb.id = "zapri_gumb";
+                zapri_gumb.type = "button";
+                zapri_gumb.textContent = "Zapri";
+
+                pozdravnoOkno.appendChild(zahvala);
+                pozdravnoOkno.appendChild(odstavek_izdelava);
+                pozdravnoOkno.appendChild(zapri_gumb);
+
+
+                // skrijemo obrazec
+                document.querySelector(".novice-obrazec").style.display ="none";
+                //prikaže se vsebina
+                let ovijalni_mali = document.getElementsByClassName("ovijalni-mali")[1];
+                ovijalni_mali.appendChild(pozdravnoOkno);
+                pozdravnoOkno.style.display = "block"; 
+                
+                zapri_gumb.addEventListener("click",function(event){
+                document.getElementById("pozdravno_okno").style.display ="none";
+            });
+                
+            });
+        </script>
+    </body>
+</ht
+
+
+
